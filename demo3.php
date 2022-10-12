b16e819c9d72eb60da4bd9d762fd23ad0fd2c07a
Ranniell Banzon <90012812+Ranniellsaur@users.noreply.github.com>
2022-10-12 15:49:44 +0800

    Add files via upload

---
+++ demo3.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+
+
+  // $month = "December";
+  // if($month=="December") echo "Its christmas timeeee!";
+
+  // $a = 1000;
+  // $b = "+1000";
+
+  // if($a != $b){
+  //   echo "1";
+  // }
+  // if($a !== $b){
+  //   echo "2";
+  // }
+
+  // $a = 2;
+  // $b = 3;
+
+  // if($a > $b) echo "a is greater than b<br>";
+  // if($a < $b) echo "a is less than b<br>";
+  // if($a >= $b) echo "a is greater than or equal to b<br>";
+  // if($a <= $b) echo "a is less than or equal to b<br>";
+
+
+  // if($a > $b){
+  //   echo "a is greater than b<br>";
+  // } elseif($a < $b){
+  //   echo "a is less than b<br>";
+  // } elseif($a >= $b){
+  //   echo "a is greater than or equal to b<br>";
+  // } elseif($a <= $b) {
+  //   echo "a is less than or equal to b<br>";
+  // }
+
+
+  // $a = 1;
+  // $b = 0;
+  // echo $a == 1 and $b == 0 , "<br>";
+  // echo $a == 1 or $b == 1, "<br>";
+  // echo $a == 1 xor $b == 0, "<br>";
+  // echo !$b
+
+  // $bank_balance = 90;
+  // if ($bank_balance<100){
+  //   $money = 1000;
+  //   $bank_balance += $money;
+  //   echo $bank_balance;
+  // } 
+
+
+  // ACTIVITY FOR TODAY /12/10/2022
+  $grade = 60;
+
+  if ($grade >= 70){
+    echo "Passed<br>";
+  }elseif ($grade >= 60){
+    echo "Remedial<br>";
+  }elseif ($grade >= 0){
+    echo "Failed";
+  }
+
+
+  ?> 
+
+
+
+
+
+  
+</body>
+</html>
